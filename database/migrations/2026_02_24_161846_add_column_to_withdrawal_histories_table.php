e8d2f8be9a2374742daf4c1fe823023dd35cfb8f
Developer <developer@gmail.com>
2026-02-24 17:22:18 +0530

    New Changed Fixed

--- database/migrations/2026_02_24_161846_add_column_to_withdrawal_histories_table.php
+++ database/migrations/2026_02_24_161846_add_column_to_withdrawal_histories_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('withdrawal_histories', function (Blueprint $table) {
-            //
+            $table->foreignId('withdraw_coin_id')->nullable()->default(null)->after('txn_id');
+            $table->string('address')->nullable()->default(null)->after('withdraw_coin_id');
+            $table->decimal('tokens', 40, 8)->nullable()->default(null)->after('address');
+            $table->decimal('token_price', 20, 4)->nullable()->default(null)->after('tokens');
+            $table->decimal('withdrawal_crypto_price', 40, 8)->nullable()->default(null)->after('token_price');
         });
     }
 
